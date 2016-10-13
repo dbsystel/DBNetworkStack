b0dd640f36d32ec66e50024bd1d4ef56d4cd0beb
Lukas Schmidt <lukas.la.schmidt@deutschebahn.com>
2016-10-13 13:25:36 +0200

    Adds documentation

--- DBNetworkStack/MultipartFormDataRepresenting.swift
+++ DBNetworkStack/MultipartFormDataRepresenting.swift
@@ -23,6 +23,18 @@
 //
 import Foundation
 
+/// Constructs `multipart/form-data` for uploads within an HTTP or HTTPS body. There are currently two ways to encode
+/// multipart form data. The first way is to encode the data directly in memory. This is very efficient, but can lead
+/// to memory issues if the dataset is too large. The second way is designed for larger datasets and will write all the
+/// data to a single file on disk with all the proper boundary segmentation. The second approach MUST be used for
+/// larger datasets such as video content, otherwise your app may run out of memory when trying to encode the dataset.
+///
+/// For more information on `multipart/form-data` in general, please refer to the RFC-2388 and RFC-2045 specs as well
+/// and the w3 form documentation.
+///
+/// - https://www.ietf.org/rfc/rfc2388.txt
+/// - https://www.ietf.org/rfc/rfc2045.txt
+/// - https://www.w3.org/TR/html401/interact/forms.html#h-17.13
 public protocol MultipartFormDataRepresenting: class {
     
     // MARK: - Properties
