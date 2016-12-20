841d91c9f05be20b866fa8eaf5ac0e2a77c13150
Christian Himmelsbach <christian.himmelsbach@deutschebahn.com>
2016-12-20 11:22:38 +0100

    Added test class for NetworkResponceProcessing protocol

---
+++ DBNetworkStackTests/NetworkResponseProcessingTests.swift
@@ -0,0 +1,54 @@
+//
+//  NetworkResponseProcessingTests.swift
+//  DBNetworkStack
+//
+//  Created by Christian Himmelsbach on 20.12.16.
+//  Copyright © 2016 DBSystel. All rights reserved.
+//
+
+import XCTest
+@testable import DBNetworkStack
+
+fileprivate class NetworkResponseProcessor : NetworkResponseProcessing {}
+
+class NetworkResponseProcessingTests: XCTestCase {
+    
+    fileprivate var processor: NetworkResponseProcessor!
+    
+    override func setUp() {
+        super.setUp()
+        processor = NetworkResponseProcessor()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testCancelError() {
+
+        // Given
+        let request = NetworkRequest(path: "", baseURLKey: "")
+        let resource = Resource(request: request, parse: { _ in return 0 })
+        let cancelledError = URLError(_nsError: NSError(domain: NSURLErrorDomain, code: NSURLErrorCancelled, userInfo: nil))
+        
+        // When
+        var result: Int? = nil
+        do {
+            result = try processor.process(response: nil, resource: resource, data: nil, error: cancelledError)
+        } catch let error as DBNetworkStackError {
+            // Then
+            switch error {
+            case .cancelled: // Excpected
+                break
+            default:
+                XCTFail("Expected cancelled error (got \(error)")
+            }
+        } catch let error {
+            XCTFail("Expected DBNetworkStackError (got \(type(of:error)))")
+        }
+        
+        XCTAssertNil(result, "Expected processing to fail")
+    }
+    
+}
