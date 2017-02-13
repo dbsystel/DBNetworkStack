350b8b77b75f98057bf7f6abf4953da44a252038
Dennis Post <dennispost@users.noreply.github.com>
2017-02-13 21:22:46 +0100

    Fixing typos

--- Source/NetworkServiceMock.swift
+++ Source/NetworkServiceMock.swift
@@ -31,21 +31,21 @@ import Foundation
 public class NetworkServiceMock: NetworkServiceProviding {
     private var onErrorCallback: ((DBNetworkStackError) -> Void)?
     private var onSuccess: ((Data) -> Void)?
-    private var onTypedSucess: ((Any) -> Void)?
+    private var onTypedSuccess: ((Any) -> Void)?
     
     public init() {}
     
-    /// Count of all stared requests
+    /// Count of all started requests
     public var requestCount: Int = 0
     /// Last executed request
-    public var lastReuqest: NetworkRequestRepresening?
+    public var lastRequest: NetworkRequestRepresening?
     /// Set this to hava a custom networktask
     public var nextNetworkTask: NetworkTaskRepresenting?
 
     @discardableResult
     public func request<T: ResourceModeling>(_ resource: T, onCompletion: @escaping (T.Model) -> Void,
                  onError: @escaping (DBNetworkStackError) -> Void) -> NetworkTaskRepresenting {
-        lastReuqest = resource.request
+        lastRequest = resource.request
         requestCount += 1
         onSuccess = { data in
             guard let result = try? resource.parse(data) else {
@@ -53,7 +53,7 @@ public class NetworkServiceMock: NetworkServiceProviding {
             }
             onCompletion(result)
         }
-        onTypedSucess = { anyResult in
+        onTypedSuccess = { anyResult in
             guard let typedResult =  anyResult as? T.Model else {
                 fatalError("Extected type of \(T.Model.self)")
             }
@@ -77,10 +77,10 @@ public class NetworkServiceMock: NetworkServiceProviding {
         }
         onErrorCallback = nil
         onSuccess = nil
-        onTypedSucess = nil
+        onTypedSuccess = nil
     }
     
-    /// Will return a sucessful request, by using the given data as a server response.
+    /// Will return a successful request, by using the given data as a server response.
     ///
     /// - Parameters:
     ///   - data: the mock response from the server. `Data()` by default
@@ -91,22 +91,22 @@ public class NetworkServiceMock: NetworkServiceProviding {
         }
         onErrorCallback = nil
         onSuccess = nil
-        onTypedSucess = nil
+        onTypedSuccess = nil
     }
     
-    /// Will return a sucessful request, by using the given type `T` as serialized result of a request.
+    /// Will return a successful request, by using the given type `T` as serialized result of a request.
     ///
     /// **Warning:** This will crash if type `T` does not match your expected ResponseType of your current request
     ///
     /// - Parameters:
     ///   - data: the mock response from the server. `Data()` by default
     ///   - count: the count how often the response gets triggerd. 1 by default
-    public func returnSucess<T>(with serializedResponse: T, count: Int = 1) {
+    public func returnSuccess<T>(with serializedResponse: T, count: Int = 1) {
         for _ in 0...count {
-            onTypedSucess?(serializedResponse)
+            onTypedSuccess?(serializedResponse)
         }
         onErrorCallback = nil
         onSuccess = nil
-        onTypedSucess = nil
+        onTypedSuccess = nil
     }
 }
