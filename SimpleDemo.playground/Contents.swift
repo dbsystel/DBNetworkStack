f3ab1ad53dd9eb8edf658e47b8f11c44280e795f
Lukas Schmidt <lukas.la.schmidt@deutschebahn.com>
2017-07-06 15:07:49 +0200

    Fix simple demo playground

--- SimpleDemo.playground/Contents.swift
+++ SimpleDemo.playground/Contents.swift
@@ -4,12 +4,12 @@ import DBNetworkStack
 import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
-let url: URL! = URL(string: "https://httpbin.org")
-let baseURLKey = "httpBin"
 
 let networkAccess = URLSession(configuration: .default)
-let networkService = NetworkService(networkAccess: networkAccess, endPoints: [baseURLKey: url])
-let request = NetworkRequest(path: "/", baseURLKey: baseURLKey)
+let networkService = NetworkService(networkAccess: networkAccess)
+
+let url: URL! = URL(string: "https://httpbin.org")
+let request = URLRequest(path: "/", baseURL: url)
 let resource = Resource(request: request, parse: { String(data: $0, encoding: .utf8) })
 
 networkService.request(resource, onCompletion: { htmlText in
