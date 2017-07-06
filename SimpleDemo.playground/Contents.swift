2ad5e2441a66026208238fc173850da0b2449a56
Lukas Schmidt <lukas.la.schmidt@deutschebahn.com>
2017-07-06 15:24:49 +0200

    Fix demo playgrounds

--- SimpleDemo.playground/Contents.swift
+++ SimpleDemo.playground/Contents.swift
@@ -3,17 +3,21 @@
 import DBNetworkStack
 import PlaygroundSupport
 
+//Setup Playground environment
 PlaygroundPage.current.needsIndefiniteExecution = true
+URLCache.shared = URLCache(memoryCapacity: 0, diskCapacity: 0, diskPath: nil)
 
+//Prepare NetworkAccess & NetworkService
 let networkAccess = URLSession(configuration: .default)
 let networkService = NetworkService(networkAccess: networkAccess)
 
-let url: URL! = URL(string: "https://httpbin.org")
-let request = URLRequest(path: "/", baseURL: url)
+//Create a Resource with a given URLRequest and parsing
+let url: URL! = URL(string: "https://bahn.de")
+let request = URLRequest(path: "/p/view/index.shtml", baseURL: url)
 let resource = Resource(request: request, parse: { String(data: $0, encoding: .utf8) })
 
 networkService.request(resource, onCompletion: { htmlText in
     print(htmlText)
-    }, onError: { _ in
-        //Handle errors
+    }, onError: { error in
+        print(error)
 })
