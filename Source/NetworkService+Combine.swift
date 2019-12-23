ea8f2bcf87c15a34c9fab3e673439cf91c515b8f
Lukas Schmidt <lukas.la.schmidt@deutschebahn.com>
2019-12-23 11:24:30 +0100

    add documentation

--- Source/NetworkService+Combine.swift
+++ Source/NetworkService+Combine.swift
@@ -12,6 +12,24 @@ import Combine
 @available(iOS 13.0, *)
 public extension NetworkService {
     
+    /**
+     Fetches a resource asynchronously from remote location. Execution of the requests starts immediately.
+     Execution happens on no specific queue. It dependes on the network access which queue is used.
+     
+     **Example**:
+     ```swift
+     let networkService: NetworkService = //
+     let resource: Resource<String> = //
+     
+     let future = networkService.request(resource: resource)
+     
+     // Handle combine future
+     ```
+     
+     - parameter resource: The resource you want to fetch.
+     
+     - returns: a future of the network request result
+     */
     func request<T>(_ resource: Resource<T>) -> Future<(T, HTTPURLResponse), NetworkError> {
         return Future<(T, HTTPURLResponse), NetworkError> { (promise) in
             self.request(resource: resource, onCompletionWithResponse: promise)
