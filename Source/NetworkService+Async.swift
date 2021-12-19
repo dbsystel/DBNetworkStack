6e445110547534767efd9290c69d69f997880739
Lukas Schmidt <lukas.la.schmidt@deutschebahn.com>
2021-12-19 18:25:38 +0100

    Adds documentation

--- Source/NetworkService+Async.swift
+++ Source/NetworkService+Async.swift
@@ -12,6 +12,21 @@ import Foundation
 @available(iOS 13.0.0, *)
 public extension NetworkService {
 
+    /**
+     Fetches a resource asynchronously from remote location. Execution of the requests starts immediately.
+
+     **Example**:
+     ```swift
+     let networkService: NetworkService = //
+     let resource: Resource<String> = //
+
+     let (result, response) = try await networkService.request(resource)
+     ```
+
+     - parameter resource: The resource you want to fetch.
+
+     - returns: a touple  containing the parsed result and the HTTP response
+     */
     @discardableResult
     func request<Result>(_ resource: Resource<Result>) async throws -> (Result, HTTPURLResponse) {
         return try await withCheckedThrowingContinuation({ coninuation in
